cdb022089c11a0baa0270a24c4270c7d958b1397
Dustin <Dustin.zhu@aishu.cn>
2023-04-26 22:51:16 +0800

    更新oauth2中间件

--- middleware/oauth2.go
+++ middleware/oauth2.go
@@ -10,31 +10,49 @@ import (
 	"strconv"
 	"strings"
 
-	"devops.aishu.cn/AISHUDevOps/AnyShareFamily/_git/GoCommon/api"
 	"devops.aishu.cn/AISHUDevOps/AnyShareFamily/_git/Hive"
 	"devops.aishu.cn/AISHUDevOps/AnyShareFamily/_git/Hive/errors"
+	"devops.aishu.cn/AISHUDevOps/AnyShareFamily/_git/Hive/infra/requests"
+	"devops.aishu.cn/AISHUDevOps/AnyShareFamily/_git/Hive/internal"
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/context"
 )
 
+type Introspection struct {
+	Active            bool                   `json:"active"`                       // Active is a boolean indicator of whether or not the presented token is currently active.
+	Audience          []string               `json:"aud,omitempty"`                // Audience contains a list of the token's intended audiences.
+	ClientID          string                 `json:"client_id,omitempty"`          // ClientID is aclient identifier for the OAuth 2.0 client that requested this token.
+	ExpiresAt         int64                  `json:"exp,omitempty"`                // Expires at is an integer timestamp, measured in the number of seconds since January 1 1970 UTC, indicating when this token will expire.
+	Extra             map[string]interface{} `json:"ext,omitempty"`                // Extra is arbitrary data set by the session.
+	IssuedAt          int64                  `json:"iat,omitempty"`                // Issued at is an integer timestamp, measured in the number of seconds since January 1 1970 UTC, indicating when this token was originally issued.
+	IssuerURL         string                 `json:"iss,omitempty"`                // IssuerURL is a string representing the issuer of this token
+	NotBefore         int64                  `json:"nbf,omitempty"`                // NotBefore is an integer timestamp, measured in the number of seconds since January 1 1970 UTC, indicating when this token is not to be used before.
+	ObfuscatedSubject string                 `json:"obfuscated_subject,omitempty"` // ObfuscatedSubject is set when the subject identifier algorithm was set to "pairwise" during authorization. It is the `sub` value of the ID Token that was issued.
+	Scope             string                 `json:"scope,omitempty"`              // Scope is a JSON string containing a space-separated list of scopes associated with this token.
+	Subject           string                 `json:"sub,omitempty"`                // Subject of the token, as defined in JWT [RFC7519]. Usually a machine-readable identifier of the resource owner who authorized this token.
+	TokenType         string                 `json:"token_type,omitempty"`         // TokenType is the introspected token's type, for example `access_token` or `refresh_token`.
+	Username          string                 `json:"username,omitempty"`           // Username is a human-readable identifier for the resource owner who authorized this token.
+}
+
 func NewAuthentication() context.Handler {
 	return func(ctx hive.Context) {
 		var err *errors.Error
-		oauth2 := api.NewOAuth2()
 		token, err := parseBearerToken(ctx.Request())
 		if err != nil {
 			errorResponse(err, ctx)
 			return
 		}
-		result, introErr := oauth2.Introspection(token, nil)
+		result, introErr := introspection(token, nil)
 		if !result.Active {
 			err = errors.UnauthorizationError(&errors.ErrorInfo{Cause: "access token expired"})
 			errorResponse(err, ctx)
 			return
 		}
-		ctx.Values().Set("userId", result.Subject)
-		ctx.Values().Set("userToken", token)
-		ctx.Values().Set("ip", result.Extra["login_ip"])
+		worker := ctx.Values().Get(internal.WorkerKey).(internal.Worker)
+		worker.Bus().Add("userId", result.Subject)
+		worker.Bus().Add("clientId", result.ClientID)
+		worker.Bus().Add("userToken", token)
+		worker.Bus().Add("ip", result.Extra["login_ip"].(string))
 		var cType, udid, visitorType string
 		if result.ClientID != result.Subject {
 			if str, ok := result.Extra["client_type"].(string); ok {
@@ -52,9 +70,9 @@ func NewAuthentication() context.Handler {
 				}
 			}
 		}
-		ctx.Values().Set("clientType", cType)
-		ctx.Values().Set("udid", udid)
-		ctx.Values().Set("visitorType", visitorType)
+		worker.Bus().Add("clientType", cType)
+		worker.Bus().Add("udid", udid)
+		worker.Bus().Add("visitorType", visitorType)
 		if err != nil {
 			err = errors.InternalServerError(&errors.ErrorInfo{Cause: "introspection failed", Detail: map[string]string{"reason": introErr.Error()}})
 			errorResponse(err, ctx)
@@ -76,6 +94,40 @@ func NewAuthentication() context.Handler {
 		}
 		ctx.Next()
 	}
+}
+
+func getHydraAdminURL() url.URL {
+	schema := os.Getenv("HYDRA_ADMIN_PROTOCOL")
+	host := os.Getenv("HYDRA_ADMIN_HOST")
+	port := os.Getenv("HYDRA_ADMIN_PORT")
+	url := url.URL{
+		Scheme: schema,
+		Host:   fmt.Sprintf("%v:%v", host, port),
+	}
+	return url
+}
+
+func getIntrospectEndpoint() string {
+	url := getHydraAdminURL()
+	url.Path = "/oauth2/introspect"
+	return url.String()
+}
+
+func introspection(token string, scopes []string) (result Introspection, err error) {
+	data := url.Values{"token": []string{token}}
+	if len(scopes) > 0 {
+		data["scope"] = []string{strings.Join(scopes, " ")}
+	}
+	introspectEndpoint := getIntrospectEndpoint()
+	req := requests.NewH2CRequest(introspectEndpoint)
+	req.AddHeader("Content-Type", "application/x-www-form-urlencoded")
+	resp := req.Post().ToJSON(&result)
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("Introspection failed, status code is %d", resp.StatusCode)
+		return
+	}
+	return
 }
 
 // parseBearerToken 解析token
